2e9ddc347c6f58db22cace671c525c467194cbee
GeoffreyULB <geoffrey.johnen@hotmail.com>
2020-07-11 14:31:46 +0200

    Add small code to get the config.ini into a dictionnary object

--- gconfig.py
+++ gconfig.py
@@ -1,2 +1,21 @@
 import configparser
 
+#Opening the config file
+#----------------------------------------------
+config = configparser.ConfigParser()
+config.read('config.ini')
+#----------------------------------------------
+
+#Analyse the config file and get the structure 
+#into config_dict
+#----------------------------------------------
+sections = config.sections()
+number_of_sections = len(sections)
+config_dict = {}
+for section in sections:
+  config_dict[section]=[]
+  for key in config[section]:
+    config_dict[section].append(key)
+
+
+
