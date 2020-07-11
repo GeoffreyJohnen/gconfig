682415d3cb568c8735c27a3b183758d52245dc94
GeoffreyULB <geoffrey.johnen@hotmail.com>
2020-07-11 20:53:50 +0200

    Add basics of the graphical interface. Displays the name of all the keys with the line. Will be constructed from that

--- gconfig.py
+++ gconfig.py
@@ -2,8 +2,8 @@ import configparser
 import sys
 
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QPushButton
-from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QFormLayout
+from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QWidget
 
 def is_bool(section,key):
@@ -13,15 +13,25 @@ def is_bool(section,key):
     is_bool = True
   return is_bool
 
+
 #Opening the config file
 #----------------------------------------------
 config = configparser.ConfigParser()
 config.read('config.ini')
 #----------------------------------------------
 
+
+
 #Analyse the config file and get the structure 
 #into config_dict
 #----------------------------------------------
+
+app = QApplication(sys.argv)
+window = QWidget()
+window.setWindowTitle('GConfig')
+layout = QFormLayout()
+
+
 sections = config.sections()
 number_of_sections = len(sections)
 config_dict = {}
@@ -29,8 +39,10 @@ for section in sections:
   config_dict[section]=[]
   for key in config[section]:
     config_dict[section].append(key)
-    print(is_bool(section,key))
+    layout.addRow(key,QLineEdit())
 
-
+window.setLayout(layout)
+window.show()
+sys.exit(app.exec_())
 
 
